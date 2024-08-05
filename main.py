bac2311ee3002e2419ce52c0693aaa1057211a4e
Zarifzz <mylegendaries75@gmail.com>
2024-08-05 15:29:39 -0700

    created config and gitignore

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello world")
+import json
+
+def load_json_key(file_path):
+    with open(file_path, 'r') as f:
+        config = json.load(f)
+        api_key = config['openai_key']
+    return api_key
+
+
+
+print(load_json_key("Api_Key.json"))
+
